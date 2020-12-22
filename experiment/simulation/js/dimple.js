c87cfabd873ea9bd768d5f473ce9937fbcb7d929
kanteshbalani <kbalani@gmail.com>
2020-12-22 17:22:42 +0530

    update simulator

--- experiment/simulation/js/dimple.js
+++ experiment/simulation/js/dimple.js
@@ -10,13 +10,13 @@ function sample_plate(){
 function combine_plate(){
     document.getElementById("display_image").src = "../images/disks_combined.PNG";
     document.getElementById("btn2").disabled = true;
-    document.getElementById("demo3").innerHTML = "3. Now make circular disk of the sample combined by putting in the brass tude";
+    document.getElementById("demo3").innerHTML = "3. Now make circular disk of the sample combined by putting in the brass tube.";
     document.getElementById("btn3").style.display = "block";
 }
 function circular_disk(){
     document.getElementById("display_image").src = "../images/disk1.PNG";
     document.getElementById("btn3").disabled = true;   
-     document.getElementById("demo4").innerHTML = "4. Now make cylinder by making a stack of disk."
+     document.getElementById("demo4").innerHTML = "4. Now Click on the Dimpling Process button on the bottom right corner."
 }
 function cylinder_disk(){
     document,getElementById("display_image").src = "../images/disks_cylinder.PNG";
